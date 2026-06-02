978526775695675f62dc54a1933093b2ec5b575a
esther-manoela <esthermanoela@gmail.com>
2026-06-01 21:23:23 -0300

    Update main.py with DataFrame exercises

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sqlite3 as sql
 
 # Create a series
 data = pd.Series([1, 2, 3, 4, 5], index=['a', 'b', 'c', 'd', 'e'])
@@ -49,3 +50,44 @@ print(df["City"].unique())
 
 # Get the number of unique values in the 'City' column
 print(df["City"].nunique())
+
+'''SAVE DATA'''
+# Save the DataFrame to a CSV file without the index
+df.to_csv('dataframe.csv', index=False)
+
+# Save the DataFrame to an Excel file without the index
+df.to_excel('dataframe.xlsx')
+
+# Save the DataFrame to a JSON file with records orientation and line-delimited format
+df.to_json('dataframe.json', orient='records', lines=True)
+
+'''LOAD DATA'''
+# Load the DataFrame from a CSV file
+df_csv = pd.read_csv('dataframe.csv')
+print(df_csv)
+
+
+'''SQL'''   
+conn = sql.connect('dataframe.db')
+
+# Save the DataFrame to a SQL database table named 'people', replacing it if it already exists, and without including the index as a column in the database         
+df.to_sql('people', conn, if_exists='replace', index=False)
+conn.close()
+
+conn =  sql.connect('dataframe.db')
+
+# Retrieve the data from the 'people' table in the SQL database and load it into a new DataFrame        
+retrieve_df = pd.read_sql('SELECT * FROM people', conn)
+print('from DB')
+print (retrieve_df)
+conn.close()
+
+print('mean: ' + str(df['Age'].mean()))
+print('median: ' + str(df['Age'].median()))
+print('mode: ' + str(df['Age'].mode()))
+print('variance: ' + str(df['Age'].var()))
+print('standard deviation: ' + str(df['Age'].std()))
+
+df['Age'].min(), df['Age'].max()
+print(abs(df['Age'].min() - df['Age'].max()))
+print(abs(df['Age'].quantile(q=0.25) - df['Age'].quantile(q=0.75)))
